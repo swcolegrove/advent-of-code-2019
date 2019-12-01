2e298eb889f9b370fc677361047e4f209ef1c53d
unknown <scott@pooping.co>
2019-12-01 02:03:28 -0500

    lint cleanup

--- 1/1.1.js
+++ 1/1.1.js
@@ -1,9 +1,10 @@
-import input from './input.js';
+import input from './input';
+
 const modules = input.split('\n');
 
 let sum = 0;
-modules.forEach((module) => {
+modules.forEach(module => {
   const fuelRequirement = Math.floor(parseInt(module) / 3) - 2;
-  sum+= fuelRequirement;
+  sum += fuelRequirement;
 });
 console.log(sum);
